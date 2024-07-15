960d24a63dcd72bafb250416ee883328c81243fe
yxu-onshape <yxu@ptc.com>
2024-07-14 21:43:43 -0400

    Add Lambda.

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>
+          这是谁家的臭猫？
+
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
